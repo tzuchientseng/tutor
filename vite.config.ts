0120905b5c1422d87e83113ab722b40d9ac9bf24
Sunny Tseng <tzuchientseng@gmail.com>
2025-07-25 14:42:14 +0800

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,15 @@
 // `npm run deploy`
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './', // https://yourusername.github.io/your-repository-name/
-  plugins: [
-    vue(),
-    vueJsx(),
-    vueDevTools(),
-  ],
+  base: '/tutor/',
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    alias: { '@': new URL('./src', import.meta.url).pathname }
+  }
 })
 
 // import { fileURLToPath, URL } from 'node:url'
